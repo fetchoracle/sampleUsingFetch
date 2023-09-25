3b26155ec3d256260b8a546f41d14fbf5571682c
Antonio Jose Lopez Morillo <antoniojlopez@gmail.com>
2023-09-25 20:50:22 -0300

    Updated documentation

--- test/testSampleFetch.js
+++ test/testSampleFetch.js
@@ -6,10 +6,10 @@ describe("Fetch", function() {
   let sampleUsingFetch;
   let fetchOracle;
   const abiCoder = new ethers.utils.AbiCoder();
-  // generate queryData and queryId for eth/usd price
-  const ETH_USD_QUERY_DATA_ARGS = abiCoder.encode(["string", "string"], ["eth", "usd"]);
-  const ETH_USD_QUERY_DATA = abiCoder.encode(["string", "bytes"], ["SpotPrice", ETH_USD_QUERY_DATA_ARGS]);
-  const ETH_USD_QUERY_ID = ethers.utils.keccak256(ETH_USD_QUERY_DATA);
+  // generate queryData and queryId for pls/usd price
+  const PLS_USD_QUERY_DATA_ARGS = abiCoder.encode(["string", "string"], ["pls", "usd"]);
+  const PLS_USD_QUERY_DATA = abiCoder.encode(["string", "bytes"], ["SpotPrice", PLS_USD_QUERY_DATA_ARGS]);
+  const PLS_USD_QUERY_ID = ethers.utils.keccak256(PLS_USD_QUERY_DATA);
 
   // Set up Fetch Playground Oracle and SampleUsingFetch
   beforeEach(async function () {
@@ -22,20 +22,20 @@ describe("Fetch", function() {
     await sampleUsingFetch.deployed();
   });
 
-  it("readEthPrice", async function() {
+  it("readPLSPrice", async function() {
     // mock value to report
     const mockValue = BigInt(2000e18);
     // convert to bytes
     const mockValueBytes = abiCoder.encode(["uint256"], [mockValue]);
     // submit value to playground
-    await fetchOracle.submitValue(ETH_USD_QUERY_ID, mockValueBytes, 0, ETH_USD_QUERY_DATA);
+    await fetchOracle.submitValue(PLS_USD_QUERY_ID, mockValueBytes, 0, PLS_USD_QUERY_DATA);
     // advance block timestamp by 15 minutes to allow our value to be retrieved
     await ethers.provider.send("evm_increaseTime", [901]);
     await ethers.provider.send("evm_mine");
     // retrieve value from playground in our sample contract
-    await sampleUsingFetch.readEthPrice();
+    await sampleUsingFetch.readPLSPrice();
     // read our saved value from the sample contract
-    const retrievedVal = await sampleUsingFetch.ethPrice();
+    const retrievedVal = await sampleUsingFetch.plsPrice();
     expect(BigInt(retrievedVal)).to.equal(mockValue);
   })
 });
